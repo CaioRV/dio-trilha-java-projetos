7a53077753a027ccb6ed13d6a5e3715951a3aac5
Caio Varago <varago079@gmail.com>
2024-05-06 19:07:44 -0300

    Sintaxe - Desafio Criação de um projeto ContaBanco que recebe dados via terminal contendo as características de conta em banco conforme atributos passados. Ideia de exercicio projetada pela DIO.

---
+++ ContaTerminal.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+public class ContaTerminal {
+    public static void main (String [] args) {
+        //To Do: Conhecer e importar a classe Scanner
+        Scanner scannerUsuario = new Scanner(System.in).useLocale(Locale.US);
+
+        String exemploNumAgencia = "(Ex.: ***-*)";
+        //To Do: Exibir as mensagens para nosso usuario
+        System.out.println("Para começamos, por favor, informe o número da Agência: " + exemploNumAgencia);
+        String numeroDAgencia = scannerUsuario.next();
+
+        System.out.println("Digite seu primeiro nome: ");
+        String primeiroNomeUsuario = scannerUsuario.next();
+
+        System.out.println("Digite seu sobrenome: ");
+        String sobrenomeUsuario = scannerUsuario.next();
+
+        System.out.println("Informe o número de sua conta: ");
+        int numeroConta = scannerUsuario.nextInt();
+
+        System.out.println("O quanto gostaria de depositar: ");
+        double saldoConta = scannerUsuario.nextDouble();
+
+        //To Do: Obter pela class Scanner os valores pelo terminal
+        System.out.println("Olá " + primeiroNomeUsuario + " " + sobrenomeUsuario + ", obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua agência é " + numeroDAgencia + ", conta número " + numeroConta + ". Seu saldo de " + saldoConta + " " + "já está disponível para saque.");
+    }
+}
